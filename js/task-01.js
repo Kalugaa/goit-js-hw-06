38060e79562f54d30428964e4e3dfcf20d430540
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-09-04 14:25:25 +0200

    1

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+/** @format */
+
+// const categoriesList = document.querySelectorAll("ul#categories .item");
+// const categoriesCount = categoriesList.length;
+
+// console.log(`Number of categories: ${categoriesCount}`);
+
+// categoriesList.forEach((item) => {
+//   const categoryName = item.querySelector("h2").textContent;
+//   const categoryItemsCount = item.querySelectorAll("ul li").length;
+
+//   console.log(`Category: ${categoryName}`);
+//   console.log(`Elements: ${categoryItemsCount}`);
+// });
