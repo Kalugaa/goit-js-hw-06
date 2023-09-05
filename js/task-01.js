259c272e3a577ffc19effc77f48b575d1bc10821
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-09-05 19:53:05 +0200

    123

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,14 @@
 /** @format */
 
-// const categoriesList = document.querySelectorAll("ul#categories .item");
-// const categoriesCount = categoriesList.length;
+const categoriesList = document.querySelectorAll("ul#categories .item");
+const categoriesCount = categoriesList.length;
 
-// console.log(`Number of categories: ${categoriesCount}`);
+console.log(`Number of categories: ${categoriesCount}`);
 
-// categoriesList.forEach((item) => {
-//   const categoryName = item.querySelector("h2").textContent;
-//   const categoryItemsCount = item.querySelectorAll("ul li").length;
+categoriesList.forEach((item) => {
+  const categoryName = item.querySelector("h2").textContent;
+  const categoryItemsCount = item.querySelectorAll("ul li").length;
 
-//   console.log(`Category: ${categoryName}`);
-//   console.log(`Elements: ${categoryItemsCount}`);
-// });
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItemsCount}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -7,19 +7,19 @@
 // Додасть елементу клас item.
 // Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
 
-// const ingredients = [
-//   "Potatoes",
-//   "Mushrooms",
-//   "Garlic",
-//   "Tomatoes",
-//   "Herbs",
-//   "Condiments",
-// ];
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
+];
 
-// const ingredientsList = document.querySelector("#ingredients");
+const ingredientsList = document.querySelector("#ingredients");
 
-// const liElements = ingredients.map((ingredient) => {
-//   return `<li class="item">${ingredient}</li>`;
-// });
+const liElements = ingredients.map((ingredient) => {
+  return `<li class="item">${ingredient}</li>`;
+});
 
-// ingredientsList.innerHTML = liElements.join("");
+ingredientsList.innerHTML = liElements.join("");
